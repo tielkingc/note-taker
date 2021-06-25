5e91dced11566c84da48aa01feb47265ac5c713f
tielkingc <tielking5@gmail.com>
2021-06-25 17:41:02 -0400

    add delete route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const oldNotes = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
+const Ids = require('ids');
 
 const PORT = process.env.PORT || 3001;
 
@@ -26,6 +27,14 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
+    const ids = new Ids();
+    let noteId = ids.next();
+
+    while (!ids.assigned(noteId)){
+        noteId = ids.next();
+    }
+
+    newNote.id = noteId;
 
     oldNotes.push(newNote);
     
@@ -36,6 +45,23 @@ app.post('/api/notes', (req, res) => {
             console.log(err);
         }
         console.log('Note saved')
+    })
+})
+
+app.delete('/api/notes/:id', (req,res) => {
+    // console.log(res)
+    for (var i = 0; i < oldNotes.length; i++) {
+        console.log(oldNotes[i].id)
+        if (oldNotes[i].id === req.params.id) {
+            oldNotes.splice(i, 1);
+        }
+    }
+
+    fs.writeFile('./db/db.json', JSON.stringify(oldNotes), function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Note deleted')
     })
 })
 
